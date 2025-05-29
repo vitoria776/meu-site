16b9d9963c43118d0221a02f5504caa4f6e52ab5
Vitória Andrade Soares <vitoria.soares4@estudante.ifto.edu.br>
2025-05-29 17:14:38 -0300

    29/05/2025

--- Css/script.js
+++ Css/script.js
@@ -1,8 +1,17 @@
-const mensagem = `Meu filho, eu te amo!
+const mensagem = `Desde que você chegou, minha vida mudou completamente.
 
-Desde que você chegou, minha vida mudou completamente.
-Você é a razão do meu sorriso e da minha felicidade.
-Te amo infinitamente!`;
+Quando vi pela primeira vez aquele rostinho tão pequenininho e tão frágil, eu
+soube que você seria minha maior paixão e o motivo do meu viver.
+
+As pessoas sempre me falaram do quão incrível e enorme é o amor de mãe, e eu sempre duvidei. 
+Pensava comigo mesma: como pode alguém amar mais que a própria vida um serzinho que mal nasceu?
+
+Eu pensava que não era possível eu sentir esse amor, porém tudo mudou quando você nasceu. 
+Hoje, você é tudo para mim: uma das coisas mais preciosas que eu tenho.
+
+Filho, eu te amo e obrigada por existir e fazer parte da minha vida.
+
+Talvez hoje você ainda não entenda, mas espero que um dia você saiba de pelo menos metade do meu amor por você. ❤️`;
 
 const textoElemento = document.getElementById('mensagemTexto');
 
